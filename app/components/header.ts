9f73971ac8588d93ed26faf5ccfce8a3e129b79e
KristienN <knyamutsaka@outlook.com>
2024-11-15 17:53:31 +0000

    fix lint

--- app/components/header.ts
+++ app/components/header.ts
@@ -1,31 +1,23 @@
 import Component from '@glimmer/component';
+import Owner from '@ember/owner';
 import { tracked } from '@glimmer/tracking';
 
-export interface HeaderSignature {
-  // The arguments accepted by the component
-  Args: {};
-  // Any blocks yielded by the component
-  Blocks: {
-    default: [];
-  };
-  // The element to which `...attributes` is applied in the component template
-  Element: null;
-}
+export interface HeaderSignature {}
 
 export default class Header extends Component<HeaderSignature> {
   jobs: string[] = ['Frontend', 'Backend', 'DevOps', 'Cloud'];
-  @tracked currentJobTitle: string = 'Frontend';
+  @tracked currentJobTitle: string = this.jobs[0] as string;
   @tracked index: number = 0;
 
-  constructor() {
-    super(...arguments);
+  constructor(owner: Owner, args: HeaderSignature) {
+    super(owner, args);
     this.updateJobTitle();
   }
 
   updateJobTitle(): void {
     setInterval(() => {
       this.index = (this.index + 1) % this.jobs.length;
-      this.currentJobTitle = this.jobs[this.index];
+      this.currentJobTitle = this.jobs[this.index] as string;
     }, 2000);
   }
 }
