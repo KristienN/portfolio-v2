47a707030c2a95eca51f3bc8a9c0176265cb041f
KristienN <knyamutsaka@outlook.com>
2024-11-15 18:47:46 +0000

    refactor workflow with yarn v2

--- tests/integration/components/frameworks-test.ts
+++ tests/integration/components/frameworks-test.ts
@@ -7,20 +7,7 @@ module('Integration | Component | frameworks', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Frameworks />`);
-
     assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Frameworks>
-        template block text
-      </Frameworks>
-    `);
-
-    assert.dom().hasText('template block text');
   });
 });
