b172047311bca5e57798c4169d36e25de78a3ba1
KristienN <knyamutsaka@outlook.com>
2024-11-24 15:50:47 +0000

    feat: Add Sample Work Page

--- app/components/sample-work.ts
+++ app/components/sample-work.ts
@@ -1,9 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export interface SampleWorkSignature {}
 
+interface Project {
+  name: string;
+  url: string;
+}
+
 export default class SampleWork extends Component<SampleWorkSignature> {
-  project: { name: string; url: string }[] = [
+  project: Project[] = [
     {
       name: 'lusk-engineering',
       url: 'http://lusk-engineering.com',
@@ -13,4 +20,14 @@ export default class SampleWork extends Component<SampleWorkSignature> {
       url: 'https://sentence-diagramming-ui.vercel.app/',
     },
   ];
+
+  @tracked activeProject: Project = this.project[1] as Project;
+  @tracked inActiveProject: Project = this.project[0] as Project;
+
+  @action
+  setActiveProject() {
+    const temp: Project = this.activeProject;
+    this.activeProject = this.inActiveProject;
+    this.inActiveProject = temp;
+  }
 }
